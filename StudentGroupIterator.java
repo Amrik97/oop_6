ac94b5ef5dfc5afe4bf8cf0ecf88a79f5349edd0
Amirkhan <amirhan75@gmail.com>
2023-06-23 15:14:57 +0600

    hm6

---
+++ StudentGroupIterator.java
@@ -0,0 +1,33 @@
+package oop_6;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentGroupIterator implements Iterator<Student> {
+    /*
+    Этот класс соответствует принципу единственной ответственности.
+    Для него определено единственное назначение
+    - итерировать объекты класса Student.
+     */
+    private int counter;
+    private final List<Student> students;
+
+    public StudentGroupIterator(StudentGroup studentGroup) {
+        this.students = studentGroup.getStudents();
+        this.counter = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return counter < students.size();
+    }
+
+    @Override
+    public Student next() {
+        if (!hasNext()){
+            throw new RuntimeException("Students");
+        }
+
+        return students.get(counter++);
+    }
+}
